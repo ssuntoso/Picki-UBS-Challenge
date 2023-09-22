0c41f889434d37f5e2689fa7b708dc7f979e94ff
Sean Michael Suntoso <ssuntoso@gmail.com>
2023-09-22 19:13:59 +0800

    Lazy developer

---
+++ routes/lazyDeveloper.py
@@ -0,0 +1,60 @@
+import json
+import logging
+import operator
+from functools import reduce
+from typing import Dict, List
+
+from flask import request
+
+from routes import app
+
+
+logger = logging.getLogger(__name__)
+
+def getFromDict(dataDict, mapList):
+  data = reduce(operator.getitem, mapList, dataDict)
+  if type(data) is dict:
+    keysList = list(data.keys())
+    keysList.sort()
+    return keysList
+  elif type(data) is list:
+    data.sort()
+    return data
+  else:
+    return ['']
+  
+def getNextProbableWords(classes: List[Dict], statements: List[str]) -> Dict[str, List[str]]:
+  new_classes = classes[0]
+  resultDict = {}
+  for i in range(1, len(classes)):
+    new_classes.update(classes[i])
+  for i in statements:
+    try:
+      keys = i.split(".")
+      if i[-1] == ".":
+        keys.remove('')
+        data = getFromDict(new_classes, keys)
+      else:
+        data = getFromDict(new_classes, keys[:-1])
+        if type(data) is list:
+          filteredList = []
+          for j in data:
+            if keys[-1] in j:
+              filteredList.append(j)
+          data = filteredList
+      resultDict[i] = data[0:5]
+    except:
+      resultDict[i] = [""]
+  return resultDict
+
+@app.route('/lazy-developer', methods=['POST'])
+def lazyDeveloper():
+    data = request.get_json()
+    classes = data["classes"]
+    statements = data["statements"]
+    output = getNextProbableWords(classes, statements)
+    print(output)
+    return json.dumps(output)
+
+
+
