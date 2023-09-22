5dba800628c58efa7fcc6f57c1a80330fc32d921
Sean Michael Suntoso <ssuntoso@gmail.com>
2023-09-22 20:35:21 +0800

    iseng

--- routes/lazyDeveloper.py
+++ routes/lazyDeveloper.py
@@ -1,5 +1,7 @@
 import json
 import logging
+import operator
+from functools import reduce
 from typing import Dict, List
 
 from flask import request
@@ -9,54 +11,41 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 
-from typing import Dict, List
-
-def getNextProbableWords(classes: List[Dict],
-                         statements: List[str]) -> Dict[str, List[str]]:
-
-  no_dicts = len(classes)  
-  ansbank = []                # storing lists of values to be returned
-  answer = {}                 # to be returned
-  items = []                  # storing lists of segmented statements
-  for i in range(len(statements)):          # loop for each statement
-    items.append(statements[i].split("."))    # initializations, divide statements
-    ansbank.append([])
-    targetdict = -1                         
-    refsearch = True                        # whether the key is successfully located. Implemented based on assumption in line 4.   
-    for j in range(len(items[i]) - 1):      # loop for each segment (except the last one)
-      refsearch = False                   
-      if targetdict == -1:                      # the search will always start with main json mapping keys
-        for d in range(no_dicts):                         # searching json mapping keys
-          if items[i][j] in classes[d]:
-            reference = items[i][j]                      
-            valuetype = type(classes[d][reference])           # check type of json key to determine method of entry
-            targetdict = d
-            d = no_dicts
-            refsearch = True
-      elif valuetype == dict or valuetype == list:            # continuing into lists or dictionaries
-        if items[i][j] in classes[targetdict][reference]:
-          if valuetype == dict:            
-            reference = classes[targetdict][reference][items[i][j]]
-          else:                            
-            reference = items[i][j]
-          for d in range(no_dicts):                           # searching json mapping keys
-            if reference in classes[d]:
-              valuetype = type(classes[d][reference])
-              targetdict = d
-              d = no_dicts
-              refsearch = True
-    if refsearch:
-      for j in classes[targetdict][reference]:                # returning probable words
-        if items[i][-1] == j[:len(items[i][-1])]:
-          ansbank[i].append(j)
-      ansbank[i].sort()                                       # ascending alphabetical order
-      while len(ansbank[i]) > 5:                              # select top 5
-        ansbank[i].pop()
-    if len(ansbank[i]) == 0:                                  # add empty string for no results
-      ansbank[i].append("")
-    answer[statements[i]] = ansbank[i]
-  # Fill in your solution here and return the correct output based on the given input
-  return answer
+def getFromDict(dataDict, mapList):
+  data = reduce(operator.getitem, mapList, dataDict)
+  if type(data) is dict:
+    keysList = list(data.keys())
+    keysList.sort()
+    return keysList
+  elif type(data) is list:
+    data.sort()
+    return data
+  else:
+    return ['']
+  
+def getNextProbableWords(classes: List[Dict], statements: List[str]) -> Dict[str, List[str]]:
+  new_classes = classes[0]
+  resultDict = {}
+  for i in range(1, len(classes)):
+    new_classes.update(classes[i])
+  for i in statements:
+    try:
+      keys = i.split(".")
+      if i[-1] == ".":
+        keys.remove('')
+        data = getFromDict(new_classes, keys)
+      else:
+        data = getFromDict(new_classes, keys[:-1])
+        if type(data) is list:
+          filteredList = []
+          for j in data:
+            if keys[-1] in j:
+              filteredList.append(j)
+          data = filteredList
+      resultDict[i] = data[0:5]
+    except:
+      resultDict[i] = [""]
+  return resultDict
 
 @app.route('/lazy-developer', methods=['POST'])
 def lazyDeveloper():
@@ -64,7 +53,8 @@ def lazyDeveloper():
     classes = data["classes"]
     statements = data["statements"]
     output = getNextProbableWords(classes, statements)
-    print(output)
+    logging.info("data sent for evaluation {}".format(data))
+    logging.info("My result :{}".format(output))
     return json.dumps(output)
 
 
