25210e2828f90d1be6ae67859a4f3f24e99a7dda
YORIKO <anandaindratan85@gmail.com>
2023-09-23 11:56:41 +0800

    fix bug

--- routes/greedyMonkey.py
+++ routes/greedyMonkey.py
@@ -13,20 +13,20 @@ def MonkeBrain(inputDict):
     max_weight = inputDict["w"]
     max_vol = inputDict["v"]
     food = inputDict["f"]
-    
+
     # Compute the value density for each item
-    food = [(w, v, val, val/(w*v)) for w, v, val in food]
-    
+    food = [(w, v, val, val/(w*v) if w*v != 0 else float('inf')) for w, v, val in food]
+
     # Sort by value density
     food.sort(key=lambda item: item[3], reverse=True)
-    
+
     total_value = 0
     for w, v, val, _ in food:
         if w <= max_weight and v <= max_vol:
             max_weight -= w
             max_vol -= v
             total_value += val
-    
+
     return total_value
 
 
