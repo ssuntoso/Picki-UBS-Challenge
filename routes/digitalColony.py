b2c5a2587a54ea7ff087bf39fa7c22e9b9106981
YORIKO <anandaindratan85@gmail.com>
2023-09-23 16:01:01 +0800

    heehee

--- routes/digitalColony.py
+++ routes/digitalColony.py
@@ -7,34 +7,35 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 
-def get_next_colony(colony, weight):
-    if len(colony) == 1:
-        return colony
+from collections import deque
 
-    num1, num2 = colony[0], colony[1]
-    sign = (num1 - num2) % 10
-    new_digit = (weight + sign) % 10
-
-    return [num1, new_digit] + get_next_colony(colony[1:], weight)
+def get_next_generation(colony, weight):
+    next_gen = [0]*10
+    for i in range(9, -1, -1):
+        if i < 9:
+            next_gen[i] += next_gen[i+1]
+        if colony[i] > 0:
+            next_gen[(i-weight)%10] += colony[i]
+            if i > 0:
+                next_gen[(i-1)%10] += colony[i]
+    return next_gen, sum(i * count for i,count in enumerate(next_gen))
 
 def get_final_weight(data):
     result = []
     for inputdata in data:
         generations = inputdata["generations"]
-        colony = list(map(int, list(inputdata["colony"])))
-        colony_sum = sum(colony)
-        
+        colony = [0]*10
+        for digit in map(int, list(inputdata["colony"])):
+            colony[digit] += 1
+        weight = sum(i * count for i,count in enumerate(colony))
         for _ in range(generations):
-            colony = get_next_colony(colony, colony_sum)
-            colony_sum = sum(colony)
-        
-        result.append(str(colony_sum))
+            colony, weight = get_next_generation(colony, weight)
+        result.append(str(weight))
     return result
-
 @app.route('/digital-colony', methods=['POST'])
 def digitalColony():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    result = getFinalWeight(data)
+    result = get_final_weight(data)
     logging.info("My result :{}".format(result))
     return json.dumps(result)
