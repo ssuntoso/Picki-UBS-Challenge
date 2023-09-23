63be32a4b9ffafe98d2a05caf22ebacfd79e3313
YORIKO <anandaindratan85@gmail.com>
2023-09-23 11:23:55 +0800

    optimization

--- routes/greedyMonkey.py
+++ routes/greedyMonkey.py
@@ -8,31 +8,56 @@ from routes import app
 logger = logging.getLogger(__name__)
 
 
-
 def MonkeBrain(inputDict):
     max_weight = inputDict["w"]
     max_vol = inputDict["v"]
     food = inputDict["f"]
-    
-    dp = [[[-1 for _ in range(max_vol+1)] for _ in range(max_weight+1)] for _ in range(len(food)+1)]
-    
-    def knapsack(i, w, v):
-        if i == 0 or w == 0 or v == 0:
-            return 0
-        if dp[i][w][v] != -1:
-            return dp[i][w][v]
-        
-        weight, vol, value = food[i-1]
-        # If the item is too heavy or too voluminous, skip it
-        if weight > w or vol > v:
-            dp[i][w][v] = knapsack(i-1, w, v)
-        else:
-            dp[i][w][v] = max(knapsack(i-1, w, v),
-                               value + knapsack(i-1, w-weight, v-vol))
-            
-        return dp[i][w][v]
-    return knapsack(len(food), max_weight, max_vol)
 
+    n = len(food)
+    left_half = food[:n//2]
+    right_half = food[n//2:]
+    
+    def compute_combinations(half):
+        n = len(half)
+        combinations = []
+        for mask in range(1 << n):
+            weight, vol, value = 0, 0, 0
+            for i in range(n):
+                if mask & (1 << i):
+                    w, v, val = half[i]
+                    weight += w
+                    vol += v
+                    value += val
+            combinations.append((weight, vol, value))
+        return combinations
+    
+    left_combinations = compute_combinations(left_half)
+    right_combinations = compute_combinations(right_half)
+    
+    right_combinations.sort()
+    
+    def compute_max_values(combinations):
+        max_values = [0 for _ in range(max_vol+1)]
+        max_val = 0
+        for w, v, val in combinations:
+            if w <= max_weight and v <= max_vol:
+                max_val = max(max_val, val)
+                max_values[v] = max(max_values[v], max_val)
+        for i in range(1, max_vol+1):
+            max_values[i] = max(max_values[i], max_values[i-1])
+        return max_values
+    
+    right_max_values = compute_max_values(right_combinations)
+    
+    max_value = 0
+    for w1, v1, val1 in left_combinations:
+        if w1 <= max_weight and v1 <= max_vol:
+            w2 = max_weight - w1
+            v2 = max_vol - v1
+            val2 = right_max_values[v2] if v2 < len(right_max_values) else 0
+            max_value = max(max_value, val1 + val2)
+    
+    return max_value
 
 
 @app.route('/greedymonkey', methods=['POST'])
