c976ecdc92b4a23ecacc6df400145b9be6c0329a
YORIKO <anandaindratan85@gmail.com>
2023-09-23 11:52:07 +0800

    Test

--- routes/greedyMonkey.py
+++ routes/greedyMonkey.py
@@ -14,25 +14,20 @@ def MonkeBrain(inputDict):
     max_vol = inputDict["v"]
     food = inputDict["f"]
     
-    dp = [[[-1 for _ in range(max_vol+1)] for _ in range(max_weight+1)] for _ in range(len(food)+1)]
+    # Compute the value density for each item
+    food = [(w, v, val, val/(w*v)) for w, v, val in food]
     
-    def knapsack(i, w, v):
-        if i == 0 or w == 0 or v == 0:
-            return 0
-        if dp[i][w][v] != -1:
-            return dp[i][w][v]
-        
-        weight, vol, value = food[i-1]
-        # If the item is too heavy or too voluminous, skip it
-        if weight > w or vol > v:
-            dp[i][w][v] = knapsack(i-1, w, v)
-        else:
-            dp[i][w][v] = max(knapsack(i-1, w, v),
-                               value + knapsack(i-1, w-weight, v-vol))
-            
-        return dp[i][w][v]
-    return knapsack(len(food), max_weight, max_vol)
-
+    # Sort by value density
+    food.sort(key=lambda item: item[3], reverse=True)
+    
+    total_value = 0
+    for w, v, val, _ in food:
+        if w <= max_weight and v <= max_vol:
+            max_weight -= w
+            max_vol -= v
+            total_value += val
+    
+    return total_value
 
 
 @app.route('/greedymonkey', methods=['POST'])
