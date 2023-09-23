405bf7d9ecd5d3d23b681c45c74539c791695e5a
Sean Michael Suntoso <ssuntoso@gmail.com>
2023-09-23 18:19:45 +0800

    update

--- routes/chinesewall.py
+++ routes/chinesewall.py
@@ -8,7 +8,7 @@ from routes import app
 logger = logging.getLogger(__name__)
 
 
-@app.route('/chinese-wall', methods=['POST'])
+@app.route('/chinese-wall', methods=['GET'])
 def chinesewall():
     result = {
         "1": "Fluffy",
