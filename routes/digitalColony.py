dffc74f95998621b8af4ad5599b4b855aa1761cb
YORIKO <anandaindratan85@gmail.com>
2023-09-23 15:40:12 +0800

    test

--- routes/digitalColony.py
+++ routes/digitalColony.py
@@ -8,17 +8,14 @@ from routes import app
 logger = logging.getLogger(__name__)
 
 def getNextColony(colony, weight):
-    colony_len = len(colony)
-    result = []
-    
-    for i in range(colony_len - 1):
-        num1, num2 = colony[i], colony[i + 1]
+    result = [colony[0]]
+
+    for i in range(1, len(colony)):
+        num1, num2 = colony[i-1], colony[i]
         sign = (num1 - num2) % 10
         newDigit = (weight + sign) % 10
-        result.extend([num1, newDigit])
-    
-    result.append(colony[-1])  # Add the last element of the original colony
-    
+        result.extend([newDigit, num2])
+
     return result
 
 def getFinalWeight(data):
@@ -26,9 +23,11 @@ def getFinalWeight(data):
     for inputdata in data:
         generations = inputdata["generations"]
         colony = list(map(int, list(inputdata["colony"])))
+        colony_sum = sum(colony)
         for i in range(generations):
-            colony = getNextColony(colony, sum(colony))
-        result.append(str(sum(colony)))
+            colony = getNextColony(colony, colony_sum)
+            colony_sum = sum(colony)
+        result.append(str(colony_sum))
     return result
 
 @app.route('/digital-colony', methods=['POST'])
